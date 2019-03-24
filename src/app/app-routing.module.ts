4e665d7543c51818ad5ec4787d8390853dbbc8fd
Mauricio Flores <contact@maurojflores.com>
2019-03-24 11:14:09 -0600

    Task list correctly renders initial list of task components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'details/:id', component: TaskDetailsComponent },
+  // { path: 'done', component: }
+  { path: '', component: TaskListComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.sass']
 })
 export class TaskListComponent implements OnInit {
+  taskList = [
+    { id: 1, title: '15min workout', status: 'unstarted', description: 'Stretch/cardio in the morning.' },
+    { id: 2, title: 'Check email', status: 'ongoing', description: 'Check and reply important messages.' },
+    { id: 3, title: 'Prepare lunch', status: 'done', description: 'Make sandwich to take to work.' },
+    { id: 4, title: 'Style home page', status: 'revising', description: 'Write CSS for home page on website.' },
+  ];
 
   constructor() { }
 
